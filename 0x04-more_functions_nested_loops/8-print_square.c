4d0083eb659034b338f46a250762f3b2718759b4
manqoba <manqoba.dube7@gmail.com>
2023-02-23 15:34:14 -0800

    de

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,6 +7,8 @@
 int print_square(int size)
 {
 int co, ro;
+
+
 if (size <= 0)
 {
 _putchar('\n');
@@ -15,7 +17,7 @@ else
 {
 for (co = 1; co <= size; co++)
 _putchar('#');
-
+{
 for (ro = 2; ro <= size; ro++)
 {
 _putchar('#');
