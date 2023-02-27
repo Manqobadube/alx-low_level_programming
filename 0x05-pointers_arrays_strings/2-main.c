e90616bda0240f4bc0a40cd40d866b8da66fe4ee
manqoba <manqoba.dube7@gmail.com>
2023-02-27 01:55:41 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int len;
+	char *str;
+
+	str = "My first strlen!";
+	len = _strlen(str);
+	printf("%d\n", len);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - lengh of the string
+ * @s: a pointer to an int
+ * Return: void that means our answer
+ */
+int _strlen(char *s)
+{
+	int u;
+
+	u = 0;
+	while (s[u] |= '\0')
+	{
+		u++;
+	}
+	return (u);
+}
