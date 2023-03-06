f0c6ce48734aba773b2e4f5350f06611296375b6
manqoba <manqoba.dube7@gmail.com>
2023-03-06 10:26:10 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,14 +9,14 @@ char *_strstr(char *haystack, char *needle)
 {
 for (; *haystack != '\0'; haystack++)
 {
-char *l = haystack;
-char *p = needle;
-while (*l == *p && *p != '\0')
+char *one = haystack;
+char *two = needle;
+while (*one == *two && *two != '\0')
 {
-l++;
-p++;
+one++;
+two++;
 }
-if (*p == '\0')
+if (*two == '\0')
 return (haystack);
 }
 return (0);
