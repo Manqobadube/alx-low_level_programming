9afda40c167a9178d5f3c35f51f289150e8b535b
manqoba <manqoba.dube7@gmail.com>
2023-03-08 11:12:03 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,3 +15,20 @@ return (i);
 return (-1);
 return (-1);
 }
+/**
+* *_sqrt_recursion - evaluate
+* *@n: interger
+* *Return: sgrt_recursion
+*/
+int _sqrt_recursion(int n)
+{
+int i = 0;
+
+if (i < 0)
+return (-1);
+
+else
+{
+return (_evaluate(i, n));
+}
+}
