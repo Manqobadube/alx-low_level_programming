94ed317e069f83779531fe4324e60e866ac62884
manqoba <manqoba.dube7@gmail.com>
2023-03-09 04:54:22 -0800

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*wildcmp - compare string
+*@s1: pointer to string
+*@s2: pointer to string2
+*Return: 0
+*/
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == '\0' && *s2 == '*')
+{
+return (wildcmp(s1, s2 + 1));
+}
+if (*s2 == '*')
+{
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+}
+else if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1));
+}
+return (0);
+}
+
