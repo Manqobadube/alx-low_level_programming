b953e6ea038d5882a299cd24e6acdb0905aeef15
manqoba <manqoba.dube7@gmail.com>
2023-03-11 22:03:53 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - print yhe number
+*@argc: number of arguments
+*@argv: array
+*Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+(void) argv;
+print("%d\n", argc - 1);
+return (0);
+}
