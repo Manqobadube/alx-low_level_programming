1ffc132069fde1516f584f2e8080320949f1c9ec
manqoba <manqoba.dube7@gmail.com>
2023-03-13 06:09:37 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*_strlen - count array
+*@s: array of elements
+*Return: 1
+*/
+int _strlen(char *s)
+{
+unsigned int i;
+
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
+/**
+*str_concat - back a pointer
+*@s1: arrat 1
+*@s2: array 2
+*Return: Always an array
+*/
+char *str_concat(char *s1, char *s2)
+{
+char *dst;
+unsigned int i, j, size;
+
+if (s1 == NULL)
+s1 = "";
+size = (_strlen(s1) + _strlen(s2) + 1);
+dst = (char *) malloc(size * sizeof(char));
+if (dst == 0)
+{
+return (NULL);
+}
+for (i = 0; *(s2 + i) != '\0'; i++)
+*(dst + i) = *(s1 + i);
+for (j = 0; *(s2 + j) != '\0'; j++)
+{
+*(dst + i) = *(s2 + j);
+i++;
+}
+return (dst);
+}
