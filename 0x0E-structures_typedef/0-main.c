c32acf45cae1d40e6b9cd5a992079e9bb8a2dc3f
manqoba <manqoba.dube7@gmail.com>
2023-03-20 07:27:12 -0700

    dog

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * main - check the code
@@ -13,6 +14,7 @@ struct dog my_dog;
 my_dog.name = "Poppy";
 my_dog.age = 3.5;
 my_dog.owner = "Bob";
+
 printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 return (0);
 }
