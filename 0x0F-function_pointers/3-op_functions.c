4dc44b1bbcabfdc9042866928e4ee9d346c57af2
manqoba <manqoba.dube7@gmail.com>
2023-03-22 06:53:20 -0700

    using diffarent oparation

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+/**
+*op_add - calculate
+*@a: first integer
+*@b: second integer
+*Return: sum of a and b
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+*op_sub - calculate
+*@a: first integer
+*@b: second integer
+*Return: subtration of a and b
+*/
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+*op_mul - calculate
+*@a: first integer
+*@b: second integer
+*Return: multiplication of a and b
+*/
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+*op_div - calculate
+*@a: first integer
+*@b: second integer
+*Return: division of a and b
+*/
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+*op_mod - calculate
+*@a: first integer
+*@b: second integer
+*Return: remainder of a and b
+*/
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
